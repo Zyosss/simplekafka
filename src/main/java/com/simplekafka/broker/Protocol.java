4bbf9c4ac8ef5ea88de23d1d8fb6c77cda64c602
FaresKoumenji <fareskoumenji@proton.me>
2026-06-30 13:00:33 +0100

    Made Produce Result Class

--- src/main/java/com/simplekafka/broker/Protocol.java
+++ src/main/java/com/simplekafka/broker/Protocol.java
@@ -142,6 +142,33 @@ public class Protocol {
         return buffer;
     }
 
+    /**
+     * Produce Result class
+     */
+    public static class ProduceResult{
 
+    private final long offset;
+    private final String error;
+
+    public ProduceResult(long offset,String error){
+        this.offset=offset;
+        this.error=error;
+    }
+
+    public long getOffset() {
+    return offset;
+    }
+    public String getError() {
+        return error;
+    }
+    public boolean isSuccess(){
+        return error==null;
+    }
+
+    }
+
+    /**
+     * Result class for Fetch
+     */
 }
 
