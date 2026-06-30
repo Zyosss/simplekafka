190bd00303cc23baa14fc6672bd35aeec5daf4a7
FaresKoumenji <fareskoumenji@proton.me>
2026-06-30 14:46:41 +0100

    Made Result Class for Metadata

--- src/main/java/com/simplekafka/broker/Protocol.java
+++ src/main/java/com/simplekafka/broker/Protocol.java
@@ -240,5 +240,37 @@ public class Protocol {
             return partitions;
         }
     }
+
+
+    /**
+     * Result Class for Metadata
+     */
+
+    public static class MetadataResult{
+        private final List<BrokerInfo> brokers;
+        private final List<TopicMetadata> topics;
+        private final String error;
+
+        public MetadataResult(List<BrokerInfo> brokers, List<TopicMetadata> topics, String error) {
+            this.brokers = brokers;
+            this.topics = topics;
+            this.error = error;
+        }
+        public List<BrokerInfo> getBrokers() {
+            return brokers;
+        }
+
+        public List<TopicMetadata> getTopics() {
+            return topics;
+        }
+
+        public String getError() {
+            return error;
+        }
+
+        public boolean isSuccess() {
+            return error == null;
+        }
+    }
 }
 
